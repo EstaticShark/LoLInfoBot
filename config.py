0b41895d398cb616b65b0f3460b1654f985101dc
Martin Chak <33637986+EstaticShark@users.noreply.github.com>
2019-07-16 16:18:48 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,20 @@
+token_dict = {
+    'discord': '<Insert Discord Token Here>',
+    'league': '<Insert League API Key Here>'
+    }
+
+region_dict = {
+                'BR': 'BR1',
+                'EUNE': 'EUN1',
+                'EUW': 'EUW1',
+                'JP': 'JP1',
+                'KR': 'KR',
+                'LAN': 'LA1',
+                'LAS': 'LA2',
+                'NA': 'NA1',
+                'OCE': 'OC1',
+                'TR': 'TR1',
+                'RU': 'RU',
+                'PBE': 'PBE1',
+                'current': 'NA'
+            } # region_dict['current'] starts with 'NA' as its default region
